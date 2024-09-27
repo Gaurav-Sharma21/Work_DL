724ddcf5ad8f99f693f4a94b9b38bc562a82b34f
Gaurav Sharma <gaurav198622@yahoo.com>
2024-09-27 09:30:30 +0200

    Prediction commit

--- models/model.py
+++ models/model.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torchvision.models as models 
 
 
+
+# In this case we are using a pretrained CNN model from torchvision.models. The main advantage of using models from the Resnet family 
+# is that it tackles the problem of vanishing gradients pretty well 
 class TumorClassifier(nn.Module):
     def __init__(self, num_classes = 4):
         super(TumorClassifier, self).__init__()
